aa524b1a360fb069e5af378b178419b98b6ec4a2
Sonu1747 <aarya0312451@gmail.com>
2026-06-20 14:09:38 +0530

    Use Brevo SMTP

--- server.js
+++ server.js
@@ -23,16 +23,12 @@ const upload = multer({
 
 /// Nodemailer SMTP setup using Gmail credentials
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  host: 'smtp.gmail.com',
+  host: "smtp-relay.brevo.com",
   port: 587,
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false,
   },
 });
 
